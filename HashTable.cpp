e112ca9e188d52358e76bcc4778744e1a955a485
nikhilmishra836 <nikhil.mishra_asp25@ashoka.edu.in>
2024-10-19 17:05:38 +0530

    oops

--- HashTable.cpp
+++ HashTable.cpp
@@ -28,7 +28,7 @@ private:
     vector<int> table; // stores elements in the hash table
     int m; // size of the table
     int elements_num; // current number of elements in the table
-    double upperloadfactor = 0.8; // threshold for resizing
+    const double upperloadfactor = 0.8; // threshold for resizing
 
     // Hash function: key mod table size
     int hashfunction(int key) {
@@ -65,7 +65,8 @@ public:
     // Insert a key into the hash table
     void insert(int key) {
         // Check for duplicates
-        if (search(key) != -1) {
+        int searchIndex = search(key);
+        if (searchIndex != -1) {
             cout << "Duplicate key insertion is not allowed" << endl;
             return;
         }
@@ -78,14 +79,23 @@ public:
         int index = hashfunction(key); // hash the key
         int i = 0;
         int newIndex = index;
+        int firstTombstone = -1;
+
         // Use quadratic probing to resolve collisions
-        while (table[newIndex] != -1 && table[newIndex] != -2) { // while not an empty spot
+        while (table[newIndex] != -1) { // while not an empty spot
+            if (table[newIndex] == -2 && firstTombstone == -1) {
+                firstTombstone = newIndex; // mark the first tombstone found
+            }
             i++;
             if (i * i >= m) { // if maximum probing limit reached
                 cout << "Max probing limit reached!" << endl;
                 return;
             }
             newIndex = (index + i * i) % m; // quadratic probing
+        }
+
+        if (firstTombstone != -1) {
+            newIndex = firstTombstone; // reuse tombstone if found
         }
 
         table[newIndex] = key; // insert the key at the calculated position
@@ -97,6 +107,7 @@ public:
         int index = hashfunction(key); // hash the key
         int i = 0;
         int newIndex = index;
+
         // Quadratic probing to find the element
         while (table[newIndex] != -1) { // continue searching until an empty slot is encountered
             if (table[newIndex] == key) { // if key is found
